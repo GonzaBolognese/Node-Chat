0ae741246dc6e78c3825921a626c2b2aa9c65bc2
GonzaBolognese <gonza.1703@gmail.com>
2024-01-25 17:46:45 -0300

    Se crearon las tablas y se dejo el chat, sincronizado y funcional

--- server/index.js
+++ server/index.js
@@ -25,11 +25,12 @@ const db = createClient({
 await db.execute(`
   CREATE TABLE IF NOT EXISTS messages(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    content TEXT
+    content TEXT,
+    username TEXT
   )
 `)
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
   console.log('a user has connected!')
 
   socket.on('disconnect', () => {
@@ -38,17 +39,34 @@ io.on('connection', (socket) => {
 
   socket.on('chat message', async (msg) => {
     let result
+    const username = socket.handshake.auth.username ?? 'anonymous'
+
     try {
       result = await db.execute({
-        sql: 'INSERT INTO messages (content) VALUES (:msg)',
-        args: { msg }
+        sql: 'INSERT INTO messages (content, username) VALUES (:msg, :username)',
+        args: { msg, username }
       })
     } catch (e) {
       console.error(e)
       return
     }
-    io.emit('chat message', msg)
+    io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
   })
+
+  if (!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: 'SELECT id, content, username FROM messages WHERE id > ?',
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+
+      results.rows.forEach(row => {
+        socket.emit('chat message', row.content, row.id.toString(), row.username)
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
 })
 
 app.use(logger('dev'))
